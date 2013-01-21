4a8752b19bc9213742f19d8aa84c035f9c44f17b
Admin <kywe665@gmail.com>
2013-01-21 13:07:50 -0700

    tested how many times lasted in loop

--- hw2/4.c
+++ hw2/4.c
@@ -5,10 +5,13 @@
 	 float f,g;
 	 f = 0.0;
 	 g = f + 1.0;
+  int i = 0;
 	 while (f != g)
 	 {
+      //printf("i: %d\n", i);
+        i++;
 	     f = g;
 	     g = f + 1.0;
 	 }
-	 printf("Does it ever get here?\n");
+	 printf("Does it ever get here? i: %d\n", i);
      }
