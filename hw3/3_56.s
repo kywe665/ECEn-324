218fe42939e2737cb417328e48e98a97ad012ea6
Kyle Weller <kywe665@gmail.com>
2013-01-28 18:25:25 -0700

    improved 3_56

--- hw3/3_56.s
+++ hw3/3_56.s
@@ -1,48 +1,31 @@
 	.file	"3_56.c"
 	.text
-	.type	loop.1595, @function
-loop.1595:
-.LFB1:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	%edi, -20(%rbp)
-	movl	%esi, -24(%rbp)
-	movl	-20(%rbp), %eax
+	.type	loop.1252, @function
+loop.1252:
+	pushl	%ebp
+	movl	%esp, %ebp
+	subl	$16, %esp
+	movl	8(%ebp), %eax
 	andl	$1, %eax
-	movl	%eax, -8(%rbp)
-	movl	$1, -4(%rbp)
+	movl	%eax, -8(%ebp)
+	movl	$1, -4(%ebp)
 	jmp	.L2
 .L3:
-	notl	-8(%rbp)
+	notl	-8(%ebp)
 .L2:
-	cmpl	$0, -4(%rbp)
+	cmpl	$0, -4(%ebp)
 	jg	.L3
-	movl	-8(%rbp), %eax
+	movl	-8(%ebp), %eax
 	leave
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE1:
-	.size	loop.1595, .-loop.1595
+	.size	loop.1252, .-loop.1252
 .globl main
 	.type	main, @function
 main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leave
-	.cfi_def_cfa 7, 8
+	pushl	%ebp
+	movl	%esp, %ebp
+	popl	%ebp
 	ret
-	.cfi_endproc
-.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (GNU) 4.4.6 20110731 (Red Hat 4.4.6-3)"
 	.section	.note.GNU-stack,"",@progbits
