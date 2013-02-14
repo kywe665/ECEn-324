7c9f9f8f44fdea205731567e79b0898c8e7e96b7
Kyle Weller <kywe665@gmail.com>
2013-02-13 23:10:15 -0700

    attempt at stack reconstruction

---
+++ lab3/phase3ASM.d
@@ -0,0 +1,11 @@
+
+phase3ASM.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 1e df 4d 52       	mov    $0x524ddf1e,%eax
+   5:	bd f8 72 ff ff       	mov    $0xffff72f8,%ebp
+   a:	68 12 8a 04 08       	push   $0x8048a12
+   f:	c3                   	ret    
---
+++ lab3/phase3ASM.s
@@ -0,0 +1,4 @@
+mov $0x524ddf1e, %eax
+mov $0xffff72f8, %ebp
+push $0x8048a12
+ret
