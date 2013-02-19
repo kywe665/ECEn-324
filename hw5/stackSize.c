c5e1b647a0d54834b35897310f405a9bffaa78ef
Kyle Weller <kywe665@gmail.com>
2013-02-19 11:19:01 -0700

    init hw5

---
+++ hw5/stackSize.c
@@ -0,0 +1,6 @@
+int main(int argc, char* argv[])
+{
+    int size[536870848];//This is maximum for -m32
+    //-m64 can allow for a much larger array, up to 9 orders of magnitude larger
+    return 0;
+}
